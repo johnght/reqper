49ebfc52922bae366eab746df2cdf44f099cf912
johnght <clmakjohnhk@gmail.com>
2015-01-12 15:23:06 +0800

    not event page

--- background.js
+++ background.js
@@ -1,78 +1,60 @@
-//VARIABLE
-var MAX_NUM_TAB = 4, INTERVAL = 1000, TIMEOUT = 60000;
-var isInAction = false;
-var rep;
-
 //OBJECT
-function reptab(tout) {
-  this.timeout = tout;
+function reptab() {
+  //expandible empty object
 }
 function repeater() {
-  this.num_tab = 0;
-  this.start_intervals = [];
-  this.request = {};//{tabId, url, method, requestBody, requestHeaders}
-  this.stepone = {isBeforeRequest:true, isBeforeSendHeaders:true, isSendHeaders:true, isCompleted:false};//prevent mains from being overidden by redirects
+  //MEMBER VARIABLE
+  this.MAX_NUM_TAB = 4;
+  this.PERIOD_PER_MIN = 1;//>=1
+  this.ALARM= "refresh";
+  this.request = {"tabId":null , "url":null, "method":null, "requestBody":null, "requestHeaders":null};
+  this.stepone = {"isBeforeRequest":true, "isBeforeSendHeaders":true, "isSendHeaders":true, "isCompleted":false};//prevent mains from being overidden by redirects
   this.reptabs = {};//{tabId:reptab(), ...}
+  //MEMBER FUNCTION
+  this.isMyTab = function(obj) {
+    return (this.reptabs[(obj.tabId).toString()]) ? true : false;
+  }
 }
 
 //CUSTOM FUNCTION
-function startrepeater() {
-  isInAction = true;
-  rep = new repeater();
-  chrome.browserAction.setBadgeText({text:"ON"});
-  chrome.browserAction.setBadgeBackgroundColor({color:[0,255,0,127]});
-}
-function endrepeater() {
-  isInAction = false;
-  rep = null;
-  chrome.browserAction.setBadgeText({text:""});
-  chrome.browserAction.setBadgeBackgroundColor({color:[0,0,0,0]});
-}
-function openTab() {
-  chrome.tabs.create({url:rep.request.url, active:false}, function(tab) {
-    var t = window.setTimeout(function(tid) { closeTab(tid); openTab();},TIMEOUT,tab.id);
-    //register the tab
-    rep.reptabs[(tab.id).toString()] = new reptab(t);
-  });
-}
 function closeTab(tid) {
   //unregister the tab
   delete rep.reptabs[tid.toString()];
   chrome.tabs.remove(tid);
 }
+function openTab() {
+  chrome.tabs.create({url:rep.request.url, active:false}, function(tab) {
+    //register the tab
+    rep.reptabs[(tab.id).toString()] = new reptab();
+  });
+}
 function startRequests() {
-  if(rep.num_tab<MAX_NUM_TAB) {
-    var t = window.setTimeout(function(){openTab();startRequests()},INTERVAL);
-    rep.num_tab++;
-    rep.start_intervals.push(t);
+  //create loop
+  for(var num_tab=0; num_tab<rep.MAX_NUM_TAB; num_tab++) {
+    openTab();
+  }
+  chrome.alarms.create(rep.ALARM, {periodInMinutes: rep.PERIOD_PER_MIN});
+}
+function noti() {
+  new Audio("alert.wav").play();
+}
+function chrome_alarms_onAlarm(alarm) {
+  if(alarm && alarm.name===rep.ALARM) {
+    for(var key in rep.reptabs) {
+      closeTab(parseInt(key));
+    }
+    for(var num_tab=0; num_tab<rep.MAX_NUM_TAB; num_tab++) {
+      openTab();
+    }
   }
 }
-function isMyTab(obj) {
-  return (rep.reptabs[(obj.tabId).toString()]) ? true : false;
-}
-function noti(){
-    new Audio("alert.wav").play();
-}
-
-//CHROME API
-chrome.browserAction.onClicked.addListener(function(tab) {
-  if(!isInAction)
-    startrepeater();
-  else
-    endrepeater();
-});
-chrome.tabs.onReplaced.addListener(function(newId, oldId) {
-  if(!isInAction)
-    return;
-  var t = rep.reptabs[oldId.toString()].timeout;
+function chrome_tabs_onReplaced(newId, oldId) {
+  rep.reptabs[newId.toString()] = new reptab();
   delete rep.reptabs[oldId.toString()];
-  rep.reptabs[newId.toString()] = new reptab(t);
-});
-chrome.webRequest.onBeforeRequest.addListener(function(obj) {
-  if(!isInAction)
-    return;
-//console.log("[onBeforeRequest_" + obj.requestId + "] " + JSON.stringify(obj));
-  if(!isMyTab(obj)) {
+}
+function chrome_webRequest_onBeforeRequest(obj) {
+  //console.log("[onBeforeRequest_" + obj.requestId + "] " + JSON.stringify(obj));
+  if(!rep.isMyTab(obj)) {
     if(rep.stepone.isBeforeRequest) {
       rep.stepone.isBeforeRequest = false;
       rep.request.tabId = obj.tabId;
@@ -85,15 +67,10 @@ chrome.webRequest.onBeforeRequest.addListener(function(obj) {
     obj.method = rep.request.method;
     obj.requestBody = rep.request.requestBody;
   }
-},
-{urls: ["<all_urls>"], types: ["main_frame"]},
-["requestBody"]
-);
-chrome.webRequest.onBeforeSendHeaders.addListener(function(obj) {
-  if(!isInAction)
-    return;
-//console.log("[onBeforeSendHeaders_" + obj.tabId + "] " + JSON.stringify(obj));
-  if(!isMyTab(obj)) {//parent
+}
+function chrome_webRequest_onBeforeSendHeaders(obj) {
+  //console.log("[onBeforeSendHeaders_" + obj.tabId + "] " + JSON.stringify(obj));
+  if(!rep.isMyTab(obj)) {//parent
     if(rep.stepone.isBeforeSendHeaders) {
       rep.stepone.isBeforeSendHeaders = false;
       rep.request.requestHeaders = obj.requestHeaders;
@@ -101,57 +78,88 @@ chrome.webRequest.onBeforeSendHeaders.addListener(function(obj) {
   } else {//main_frame + my tab
     obj.requestHeaders = rep.request.requestHeaders;
   }
-},
-{urls: ["<all_urls>"], types: ["main_frame"]},
-["requestHeaders"]
-);
-chrome.webRequest.onSendHeaders.addListener(function(obj) {
-  if(!isInAction)
-    return;
-  if(!isMyTab(obj)) {
+}
+function chrome_webRequest_onSendHeaders(obj) {
+  if(!rep.isMyTab(obj)) {
     if(rep.stepone.isSendHeaders) {
       rep.stepone.isSendHeaders = false;
       startRequests();
     }
   }
-},
-{urls: ["<all_urls>"], types: ["main_frame"]}
-);
-//chrome.webRequest.onErrorOccurred.addListener(function(obj) {
-//  if(!isInAction)
-//    return;
-////console.log("[onErrorOccurred] " + JSON.stringify(obj));
-//},
-//{urls: ["<all_urls>"], types: ["main_frame"]}
-//);
-chrome.webRequest.onCompleted.addListener(function(obj) {
-  if(!isInAction || rep.stepone.isCompleted)
+}
+function chrome_webRequest_onCompleted(obj) {
+  if(rep.stepone.isCompleted)
     return;
-  if(obj.tabId===rep.request.tabId || isMyTab(obj)) {
-//console.log("[onCompleted] " + JSON.stringify(obj));
+  if(obj.tabId===rep.request.tabId || rep.isMyTab(obj)) {
+  //console.log("[onCompleted] " + JSON.stringify(obj));
     var regex_success = /2\d\d/;
     if(regex_success.test(obj.statusCode)) {
       rep.stepone.isCompleted = true;
-      //remove not-started-yet intervals
-      for(var i=0; i<rep.start_intervals.length; i++) {
-        clearTimeout(rep.start_intervals[i]);
-      }
-      //remove all timeouts and not-completed tabs
-      for(key in rep.reptabs) {
-        clearTimeout(rep.reptabs[key].timeout);
-        var k = parseInt(key);
-        if(obj.tabId!==k)
-          chrome.tabs.remove(k);
+      //remove alarm and not-completed tabs
+      chrome.alarms.clearAll(function(wasCleared){});
+      for(var key in rep.reptabs) {
+        if(obj.tabId!==parseInt(key)) {
+          chrome.tabs.remove(parseInt(key));
+        }
       }
       if(obj.tabId!==rep.request.tabId)
         chrome.tabs.remove(rep.request.tabId);
-      endrepeater();
-      //notification
       noti();
-    } else {
-      //non-successful: do nothing, continues the loop
+      endrepeater(false);
     }
   }
-},
-{urls: ["<all_urls>"], types: ["main_frame"]}
-);
+}
+//function chrome_webRequest_onErrorOccurred(obj) {
+//  console.log("[onErrorOccurred] " + JSON.stringify(obj));
+//}
+function startrepeater() {
+  chrome.browserAction.setBadgeText({text:"ON"});
+  chrome.browserAction.setBadgeBackgroundColor({color:[0,255,0,127]});
+  rep = new repeater();
+  chrome.alarms.onAlarm.addListener(chrome_alarms_onAlarm);
+  chrome.tabs.onReplaced.addListener(chrome_tabs_onReplaced);
+  chrome.webRequest.onBeforeRequest.addListener(chrome_webRequest_onBeforeRequest,
+    {urls: ["<all_urls>"], types: ["main_frame"]},
+    ["requestBody"]
+  );
+  chrome.webRequest.onBeforeSendHeaders.addListener(chrome_webRequest_onBeforeSendHeaders,
+    {urls: ["<all_urls>"], types: ["main_frame"]},
+    ["requestHeaders"]
+  );
+  chrome.webRequest.onSendHeaders.addListener(chrome_webRequest_onSendHeaders,
+    {urls: ["<all_urls>"], types: ["main_frame"]}
+  );
+  chrome.webRequest.onCompleted.addListener(chrome_webRequest_onCompleted,
+    {urls: ["<all_urls>"], types: ["main_frame"]}
+  );
+//  chrome.webRequest.onErrorOccurred.addListener(chrome_webRequest_onErrorOccurred,
+//    {urls: ["<all_urls>"], types: ["main_frame"]}
+//  );
+}
+function endrepeater(isAbort) {
+  if(isAbort) {
+    for(var key in rep.reptabs) {
+      chrome.tabs.remove(parseInt(key));
+    }
+  }
+  chrome.browserAction.setBadgeText({text:""});
+  chrome.browserAction.setBadgeBackgroundColor({color:[0,0,0,0]});
+  chrome.tabs.onReplaced.removeListener(chrome_tabs_onReplaced);
+  chrome.webRequest.onBeforeRequest.removeListener(chrome_webRequest_onBeforeRequest);
+  chrome.webRequest.onBeforeSendHeaders.removeListener(chrome_webRequest_onBeforeSendHeaders);
+  chrome.webRequest.onSendHeaders.removeListener(chrome_webRequest_onSendHeaders);
+  chrome.webRequest.onCompleted.removeListener(chrome_webRequest_onCompleted);
+  chrome.alarms.clearAll(function(wasCleared){});
+  chrome.alarms.onAlarm.removeListener(chrome_alarms_onAlarm);
+//  chrome.webRequest.onErrorOccurred.removeListener(chrome_webRequest_onErrorOccurred);
+  rep = null;
+}
+
+//ENTRANCE
+var rep;
+chrome.browserAction.onClicked.addListener(function(tab) {
+  if(!chrome.tabs.onReplaced.hasListeners())
+    startrepeater();
+  else
+    endrepeater(true);
+});
